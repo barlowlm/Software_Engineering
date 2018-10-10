53c9315d6926d02eb726fd1b7ed0d78bd2d0f8e2
Mathie Barlow-Ladias <barlowlm@tcd.ie>
2018-10-10 18:57:26 +0100

    LCA of a DAG

--- LCA.py
+++ LCA.py
@@ -54,3 +54,47 @@ t = lca(root, n1, n2)
 print "LCA of %d and %d is %d" %(n1, n2, t.data) 
   
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
+
+graph = {'A': ['B', 'C'],
+             'B': ['C', 'D'],
+             'C': ['D'],
+             'D': ['C'],
+             'E': ['F'],
+             'F': ['C']}
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+
+    if not graph.has_key(start):
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+   
+
+
+# find path graph
+print(find_all_paths(graph, 'A', 'D')) 
+print(find_shortest_path(graph, 'A', 'D'))
+
