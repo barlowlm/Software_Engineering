1a6268adaa53cfffc3b8f2e0fa826191d5945688
Mathie Barlow-Ladias <barlowlm@tcd.ie>
2018-10-10 19:08:22 +0100

    new bfs functions added

--- LCA.py
+++ LCA.py
@@ -91,6 +91,34 @@ def find_shortest_path(graph, start, end, path=[]):
                 if not shortest or len(newpath) < len(shortest):
                     shortest = newpath
     return shortest
+
+#bfs
+
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+    vertex = queue.pop(0)
+    if vertex not in visited:
+        visited.add(vertex)
+        queue.extend(graph[vertex] - visited)
+return visited
+
+# bfs
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
    
 
 
