c06718a67cda2e75722840a94b0af107e3bb53db
barlowlm <barlowlm@tcd.ie>
2018-11-12 16:05:36 +0000

    acces token interigate

--- Python_Interigation/interigation.py
+++ Python_Interigation/interigation.py
@@ -1,13 +1,13 @@
 from github import Github
 
 # using username and password
-#g = Github("user", "password")
+#g = Github("barlowlm", "password")
 
 # or using an access token
-g = Github("0bbc2698d53a08d1693bddd39bdc3ed10b0263e5")
+g = Github("d1045bd2897fb8b960e5617dde1c9f78ff2708f5")
 
 # Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", login_or_token="0bbc2698d53a08d1693bddd39bdc3ed10b0263e5")
+#g = Github(base_url="https://{hostname}/api/v3", login_or_token="0bbc2698d53a08d1693bddd39bdc3ed10b0263e5")
 
 for repo in g.get_user().get_repos():
     print(repo.name)
