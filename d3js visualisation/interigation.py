99a0cbbe9f5687720119250b551198470e0503c5
barlowlm <barlowlm@tcd.ie>
2018-12-03 19:08:17 +0000

    d3py

--- d3js visualisation/interigation.py
+++ d3js visualisation/interigation.py
@@ -1,35 +1,21 @@
 from github import Github
 import json
+import requests
+import csv
+import io
+#import NoapiInterigation
+import d3py
+import pandas
+import numpy as np
+#import d3.v4.js
+
 #writing a jason file
 # -*- coding: utf-8 -*-
 # Make it work for Python 2+3 and with Unicode
-import io
-try:
-    to_unicode = unicode
-except NameError:
-    to_unicode = str
 
-# Define data:  this should be done in the loop below
-data = {'a list': [1, 42, 3.141, 1337, 'help', u'€'],
-        'a string': 'bla',
-        'another dict': {'foo': 'bar',
-                         'key': 'value',
-                         'the answer': 42}}
-
-# Write JSON file
-with io.open('data.json', 'w', encoding='utf8') as outfile:
-    str_ = json.dumps(data,
-                      indent=4, sort_keys=True,
-                      separators=(',', ': '), ensure_ascii=False)
-    outfile.write(to_unicode(str_))
-
-# Read JSON file
-with open('data.json') as data_file:
-    data_loaded = json.load(data_file)
-
-print(data == data_loaded)
+#print(data == data_loaded)
 # using username and password
-#g = Github("barlowlm", "Spassword")
+#g = Github("barlowlm", "")
 
 # or using an access token
 g = Github("d1045bd2897fb8b960e5617dde1c9f78ff2708f5")
@@ -37,6 +23,9 @@ g = Github("d1045bd2897fb8b960e5617dde1c9f78ff2708f5")
 # Github Enterprise with custom hostname
 #g = Github(base_url="https://{hostname}/api/v3", login_or_token="0bbc2698d53a08d1693bddd39bdc3ed10b0263e5")
 
+
+
+#APIv3 utilized to print directory
 for repo in g.get_user().get_repos():
     print(repo.name)
 i=0
@@ -44,7 +33,63 @@ for repo in g.get_user().get_repos():
     i+=1
     print(repo.name)
     print(repo.git_commits_url + str(i))
-    #repo.edit(has_wiki=False)
     # to see all the available attributes and methods
     print(dir(repo))
     print("\n")
+
+for repo in g.get_user().get_repos():
+    print(repo.get_commits().totalCount)
+
+
+#
+#scrapping to find commits per repository
+#
+
+#total = 0
+#print('\n')
+#print(g.get_user())
+##print('\n')
+
+#for repo in NoapiInterigation.count_user_commits('barlowlm'):
+#   print ("Repo `%(name)s` has %(num_commits)d commits, size %(size)d." % repo)
+    
+#total += repo['num_commits']
+
+
+#print ("Total commits: %d" % total)
+
+
+
+## input into a csv
+
+with open('commits.csv', mode='w') as csv_file:
+    fieldnames = ['Repo_Name', 'Commits', 'size']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for repo in g.get_user().get_repos():
+        print(repo.get_commits().totalCount)
+        writer.writerow({'Repo_Name': repo.name , 'Commits': repo.get_commits().totalCount , 'size': repo.get_commits().totalCount})
+    
+
+
+
+## make a flower using jason data
+
+
+
+# some test data
+T = 100
+# this is a data frame with three columns (we only use 2)
+df = pandas.DataFrame({
+    "time" : range(T),
+    "pressure": np.random.rand(T),
+    "temp" : np.random.rand(T)
+})
+## build up a figure, ggplot2 style
+# instantiate the figure object
+fig = d3py.PandasFigure(df, name="basic_example", width=300, height=300) 
+# add some red points
+fig += d3py.geoms.Point(x="pressure", y="temp", fill="red")
+# writes 3 files, starts up a server, then draws some beautiful points in Chrome
+fig.show() 
